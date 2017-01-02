ec533941182d432c68e0130b73e72260053b5c1f
kulchenkoalex <alexej07@bk.ru>
2017-01-02 22:51:36 +0200

    Создание файла для вывода статей

---
+++ views/articles.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <title>Мой Блог</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div>
+            <h1>Мой Блог</h1>
+        <div>
+            <?php foreach($articles as $a): ?>
+            <div class="article">
+                <h3><a href="article.php?id=<?=$a['id']?>"><?=$a['title']?></a></h3>
+            <em>Опубликовано <?=$a['description']?></em>
+            <p><?=$a['content']?></p>
+            </div>
+        <?php endforeach ?>
+        </div>
+        <footer>
+            <p>Мой первый блог
+            <br>
+            Copyright &copy 2016
+            </p>
+        </footer>
+        </div>
+    </body>
+</html>
