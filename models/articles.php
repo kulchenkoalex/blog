c8a439227be45107239e951164d9f5aaf073fd09
kulchenkoalex <alexej07@bk.ru>
2017-01-02 20:31:43 +0200

    Создание файла с функциями для управления записями

---
+++ models/articles.php
@@ -0,0 +1,20 @@
+<?php
+
+    function articles_all(){
+        
+    }
+
+function articles_get($id){
+        
+    }
+function articles_new($title, $description, $content){
+        
+    }
+function articles_edit($id, $title, $description, $content){
+        
+    }
+function articles_delete($id){
+        
+    }
+
+?>
