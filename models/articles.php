7605bb9f507602c31d36d4246d7272af65ed16cf
kulchenkoalex <alexej07@bk.ru>
2017-01-03 00:32:30 +0200

    Написание обновленных функций вывода выбранной статьи и всех статей (информация берется с БД)

--- models/articles.php
+++ models/articles.php
@@ -1,18 +1,37 @@
 <?php
 
-    function articles_all(){
-        $art1=["id"=>1, "title"=>"Title1", "description"=>"2017-01-02", "content"=>"Content1"];
-         $art2=["id"=>2, "title"=>"Title2", "description"=>"2017-01-02", "content"=>"Content2"];
+    function articles_all($link)
+    {
+        $query = "SELECT * FROM articles ORDER BY id DESC";
+        $result = mysqli_query($link, $query);
         
-        $arr[0] = $art1;
-        $arr[1] = $art2;
+        if (!$result)
+            die(mysqli_error($link));
         
-        return $arr;
+        $n = mysqli_num_rows($result);
+        $articles = array();
+        
+        for ($i=0; $i<$n; $i++)
+        {
+            $row = mysqli_fetch_assoc($result);
+            $articles[] = $row;
+        }
+        
+        return $articles;
     }
+       
 
-function articles_get($id){
-    return ["id"=>1, "title"=>"Простой заголовок", "description"=>"2017-01-02", "content"=>"Тут будет написана статья"];
+function articles_get($link, $id_article)
+    {
+        $query = sprintf("SELECT * FROM articles WHERE id=%d", (int)$id_article);
+        $result = mysqli_query($link, $query);
         
+        if (!$result)
+            die(mysqli_error($link));
+        
+        $article = mysqli_fetch_assoc($result);
+        
+        return $article;
     }
 function articles_new($title, $description, $content){
         
