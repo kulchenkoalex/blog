59b6bdf8d0b057a4da19590e3aac792e8d10bdea
kulchenkoalex <alexej07@bk.ru>
2017-01-04 22:27:49 +0200

    Создание функции для добавления пользователя в админке

--- models/users.php
+++ models/users.php
@@ -35,5 +35,39 @@ function users_get($link, $id_users)
         return $users;
     }
 
+function users_new($link, $name, $surname, $number, $email, $role, $login, $password)
+    {
+        $name = trim($name);
+        $surname = trim($surname);
+        $number = trim($number);
+        $email = trim($email);
+        $role = trim($role);
+        $login = trim($login);
+        $password = trim($password);
+        
+        if ($name == '' or $surname == '' or $number == '' or $email == '' 
+            or $login == '' or $password == '')
+            return false;
+        if ($role == '')
+            $role ="пользователь";
+        
+        $t = "INSERT INTO users (name, surname, number, email, role, login, password) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')";
+    
+        $query = sprintf($t,  mysqli_real_escape_string($link, $name),
+                         mysqli_real_escape_string($link,$surname),
+                         mysqli_real_escape_string($link,$number),
+                        mysqli_real_escape_string($link,$email),
+                        mysqli_real_escape_string($link,$role),
+                        mysqli_real_escape_string($link,$login),
+                        mysqli_real_escape_string($link,$password));
+        
+        echo $query;
+        $result = mysqli_query($link, $query);
+        
+        if (!$result)
+            die(mysqli_error($link));
+        
+        return true;
+    }
 
 ?>
