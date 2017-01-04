fbac70f0e58953782113af52e7a3cda0d328afbe
kulchenkoalex <alexej07@bk.ru>
2017-01-04 22:28:50 +0200

    Выполняемые действия при добавлении пользователя (редирект на страницы и т.д.)

--- admin/users.php
+++ admin/users.php
@@ -5,7 +5,27 @@
 
     $link = db_connect();
  
-    $users = users_all($link);
+
+
+
+if (isset($_GET['action']))
+        $action = $_GET['action'];
+    else 
+        $action = "";
+ 
+    if ($action == "add")
+    {
+        if(!empty($_POST))
+        {
+            users_new($link, $_POST['name'], $_POST['surname'], $_POST['number'], $_POST['email'], $_POST['role'], $_POST['login'], $_POST['password']);
+            header("Location: users.php");
+        }
+        include("../views/user.php");
+    }
+    else
+    {
+            $users = users_all($link);
   include("../views/users.php");
-    
+
+    }
 ?>
