9f3263a62745d697c328250566ff7ec9d35ab764
kulchenkoalex <alexej07@bk.ru>
2017-01-04 22:44:59 +0200

    Написание функций для редактирования и удаления пользователей

--- models/users.php
+++ models/users.php
@@ -48,9 +48,7 @@ function users_new($link, $name, $surname, $number, $email, $role, $login, $pass
         if ($name == '' or $surname == '' or $number == '' or $email == '' 
             or $login == '' or $password == '')
             return false;
-        if ($role == '')
-            $role ="пользователь";
-        
+
         $t = "INSERT INTO users (name, surname, number, email, role, login, password) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')";
     
         $query = sprintf($t,  mysqli_real_escape_string($link, $name),
@@ -68,6 +66,57 @@ function users_new($link, $name, $surname, $number, $email, $role, $login, $pass
             die(mysqli_error($link));
         
         return true;
+    }
+
+function users_edit($link, $id, $name, $surname, $number, $email, $role, $login, $password)
+    {
+        $name = trim($name);
+        $surname = trim($surname);
+        $number = trim($number);
+        $email = trim($email);
+        $role = trim($role);
+        $login = trim($login);
+        $password = trim($password);
+        $id = (int)$id;
+        
+        if ($name == '' or $surname == '' or $number == '' or $email == '' 
+            or $login == '' or $password == '')
+            return false;
+        
+        $sql = "UPDATE users SET name='%s', surname='%s', number='%s', email='%s', role='%s', login='%s', password='%s' WHERE id='%d'";
+        
+        $query = sprintf($sql, mysqli_real_escape_string($link, $name),
+                         mysqli_real_escape_string($link,$surname),
+                         mysqli_real_escape_string($link,$number),
+                        mysqli_real_escape_string($link,$email),
+                        mysqli_real_escape_string($link,$role),
+                        mysqli_real_escape_string($link,$login),
+                        mysqli_real_escape_string($link,$password), $id);
+        
+        $result = mysqli_query($link, $query);
+        
+        if (!$result)
+            die(mysqli_error($link));
+        
+        return mysqli_affected_rows($link);
+        
+    }
+
+
+function users_delete($link, $id)
+    {
+       $id = (int)$id;
+        
+        if($id == 0)
+            return false;
+        
+        $query = sprintf("DELETE FROM users WHERE id='%d'", $id);
+        $result = mysqli_query($link, $query);
+        
+        if (!$result)
+            die(mysqli_error($link));
+        
+        return mysqli_affected_rows($link);
     }
 
 ?>
