5bbf151c0344aeb30f4a8491ee236204223ec586
kulchenkoalex <alexej07@bk.ru>
2017-01-06 00:32:29 +0200

    Валидация данных при редактировании и добавлении пользователя

--- views/user.php
+++ views/user.php
@@ -11,12 +11,12 @@
             <h1>Мой Блог</h1>
         <div class="container">
             <form method="post" action="../admin/users.php?action=<?=$_GET['action']?>&id=<?=$_GET['id']?>">
-               <br><input type="text" name="login" value="<?=$users['login']?>" placeholder="Логин" maxlength="20" pattern="[A-Za-z-0-9]{3,20}" title="Не менее 3 и неболее 20 латынских символов или цифр." required>
-<br><input type="text" name="password" value="<?=$users['password']?>"placeholder="Пароль" maxlength="15" pattern="[A-Za-z-0-9]{5,15}" title="Не менее 5 и неболее 15 латынских символов или цифр." required>
+              <br><input type="text" name="login" value="<?=$users['login']?>" placeholder="Логин" maxlength="20" pattern="[A-Za-z-0-9]{3,20}" title="Не менее 3 и неболее 20 латинских символов или цифр." required>
+<br><input type="password" name="password" value="<?=$users['password']?>" placeholder="Пароль" maxlength="100" pattern="[A-Za-z-0-9]{5,100}" title="Не менее 5 символов." required>
     <br><input type="email" name="email" value="<?=$users['email']?>" placeholder="E-Mail" required>
-<br><input type="text" name="name" value="<?=$users['name']?>" placeholder="Имя" maxlength="20" pattern="[А-Яа-яЁё-A-Za]{4,10}" title="Не менее 4 и неболее 20 латынских символов или цифр." required>
-    <br><input type="text" name="surname" value="<?=$users['surname']?>" placeholder="Фамилия" maxlength="20" pattern="[А-Яа-яЁё-A-Za]{4,20}" title="Не менее 4 и неболее 20 латынских символов или цифр." required>
-    <br><input type="text" name="number" value="<?=$users['number']?>" placeholder="Номер телефона" maxlength="12" pattern="{4,12}" title="Не менее 4 и неболее 12 латынских символов или цифр." required>
+<br><input type="text" name="name" value="<?=$users['name']?>" placeholder="Имя" maxlength="20" pattern="[А-Яа-яЁё-A-Za-z]{3,20}" title="Не менее 3 и неболее 20 символов." required>
+    <br><input type="text" name="surname" value="<?=$users['surname']?>" placeholder="Фамилия" maxlength="20" pattern="[А-Яа-яЁё-A-Za-z]{3,20}" title="Не менее 3 и неболее 20 символов." required>
+    <br><input type="tel" name="number" value="<?=$users['number']?>" placeholder="(050) 999-99-99" pattern="\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}" title="Формат ввода (050) 121-34-57." required>
     <br><select size="1" name="role" value="<?=$users['role']?>"><option value="Пользователь">Пользователь</option><option value="Администратор">Администратор</option></select>
     <br><br>
 <input type="submit" name="enter" value="Сохранить"> <input type="reset" value="Очистить">
--- views/user_add.php
+++ views/user_add.php
@@ -11,12 +11,12 @@
             <h1>Мой Блог</h1>
         <div class="container">
             <form method="post" action="../admin/users.php?action=add">
-               <br><input type="text" name="login" placeholder="Логин" maxlength="20" pattern="[A-Za-z-0-9]{3,20}" title="Не менее 3 и неболее 20 латынских символов или цифр." required>
-<br><input type="text" name="password" placeholder="Пароль" maxlength="15" pattern="[A-Za-z-0-9]{5,15}" title="Не менее 5 и неболее 15 латынских символов или цифр." required>
+               <br><input type="text" name="login" placeholder="Логин" maxlength="20" pattern="[A-Za-z-0-9]{3,20}" title="Не менее 3 и неболее 20 латинских символов или цифр." required>
+<br><input type="password" name="password" placeholder="Пароль" maxlength="100" pattern="[A-Za-z-0-9]{5,100}" title="Не менее 5 символов." required>
     <br><input type="email" name="email" placeholder="E-Mail" required>
-<br><input type="text" name="name" placeholder="Имя" maxlength="20" pattern="[А-Яа-яЁё-A-Za]{4,10}" title="Не менее 4 и неболее 20 латынских символов или цифр." required>
-    <br><input type="text" name="surname" placeholder="Фамилия" maxlength="20" pattern="[А-Яа-яЁё-A-Za]{4,20}" title="Не менее 4 и неболее 20 латынских символов или цифр." required>
-    <br><input type="text" name="number" placeholder="Номер телефона" maxlength="12" pattern="{4,12}" title="Не менее 4 и неболее 12 латынских символов или цифр." required>
+<br><input type="text" name="name" placeholder="Имя" maxlength="20" pattern="[А-Яа-яЁё-A-Za-z]{3,20}" title="Не менее 3 и неболее 20 символов." required>
+    <br><input type="text" name="surname" placeholder="Фамилия" maxlength="20" pattern="[А-Яа-яЁё-A-Za-z]{3,20}" title="Не менее 3 и неболее 20 символов." required>
+    <br><input type="tel" name="number" placeholder="(050) 999-99-99" pattern="\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}" title="Формат ввода (050) 121-34-57." required>
     <br><select size="1" name="role"><option value="Пользователь">Пользователь</option><option value="Администратор">Администратор</option></select>
     <br><br>
 <input type="submit" name="enter" value="Добавить"> <input type="reset" value="Очистить">
